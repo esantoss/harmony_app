0f46c1985bb6a4f4e60bd7006daa95d001683526
esantoss <root@Erick-Ryzen-PC.localdomain>
2021-12-20 16:17:16 -0500

    updating code

--- harmony_app.py
+++ harmony_app.py
@@ -1,55 +1,65 @@
 import json
 
-def get_index(curr_index):
-  if curr_index + 1 >= len(scale):
-    curr_index = 0
-  else:
-    curr_index += 1
-  return curr_index
+class HarmonyApp:
 
-def get_interval_start(first_note, interval_offset):
-  start_index = scale.index(first_note)
-  curr_index = start_index
-  for i in range(interval_offset):
-    curr_index = get_index(curr_index)
-  return curr_index
+  scale = ["A", "B", "C#", "D", "E", "F#", "G#"]
+  notes = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
 
-def get_scale_notes(first_note, interval_start_index, total_notes):
-  scale_notes = []
+  intervals = {
+    'Major Second': 1,
+    'Major Third': 2, 
+    'Perfect Fourth': 3,
+    'Perfect Fifth': 4,
+    'Major Sixth': 5,
+    'Major Seventh': 6
+  }
 
-  curr_index = interval_start_index
-  for i in range(total_notes):
-    scale_notes.append(scale[curr_index])
-    curr_index = get_index(curr_index)
-  return scale_notes
+  def __init__(self):
+    pass
 
-scale = ["A", "B", "C#", "D", "E", "F#", "G#"]
-notes = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
+  def get_index(self, curr_index):
+    if curr_index + 1 >= len(self.scale):
+      curr_index = 0
+    else:
+      curr_index += 1
+    return curr_index
 
-intervals = {
-  'Major Second': 1,
-  'Major Third': 2, 
-  'Perfect Fourth': 3,
-  'Perfect Fifth': 4,
-  'Major Sixth': 5,
-  'Major Seventh': 6
-}
+  def get_interval_start(self, first_note, interval_offset):
+    start_index = self.scale.index(first_note)
+    curr_index = start_index
+    for i in range(interval_offset):
+      curr_index = self.get_index(curr_index)
+    return curr_index
 
-with open("input.json", "r") as input:
-  input_data = json.loads(input.read())
+  def get_scale_notes(self, first_note, interval_start_index, total_notes):
+    scale_notes = []
+    curr_index = interval_start_index
+    for i in range(total_notes):
+      scale_notes.append(self.scale[curr_index])
+      curr_index = self.get_index(curr_index)
+    return scale_notes
 
-phrases = input_data['phrases']
-for phrase in phrases:
-  try:
-    interval_offset = intervals[phrase['interval']]
-    input_notes = phrase['notes']
-    scale_type = phrase['scale']
-    total_notes = len(input_notes)
-    first_note = input_notes[0]
-    interval_start_index = get_interval_start(first_note, interval_offset)
-    harmony_notes = get_scale_notes(first_note, interval_start_index, total_notes)
-  except Exception:
-    print(f"ERROR!! with input -> {input_notes}")
+  def main(self):
+    with open("input.json", "r") as input:
+      input_data = json.loads(input.read())
 
-  print(f"Riff Notes: {input_notes}")
-  print(f"Harmony Notes: {harmony_notes}")
+    phrases = input_data['phrases']
+    for phrase in phrases:
+      interval_offset = self.intervals[phrase['interval']]
+      input_notes = phrase['notes']
+      scale_type = phrase['scale']
+      total_notes = len(input_notes)
+      first_note = input_notes[0]
+      harmony_notes = None
+      try:
+        interval_start_index = self.get_interval_start(first_note, interval_offset)
+        harmony_notes = self.get_scale_notes(first_note, interval_start_index, total_notes)
+      except Exception as e:
+        print(f"ERROR!! with input -> {input_notes}")
+        print(f"ERROR is: {e}")
+
+      print(f"Riff Notes: {input_notes}")
+      print(f"Harmony Notes: {harmony_notes}")
+
+if __name__ == '__main__':
+  HarmonyApp().main()
