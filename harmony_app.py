7974da720fbf0f6d704ed084ca80ca3ec795bced
esantoss <root@Erick-Ryzen-PC.localdomain>
2021-12-20 15:54:48 -0500

    updating code

--- harmony_app.py
+++ harmony_app.py
@@ -1,9 +1,19 @@
 import json
-
-def get_scale_notes(start, total_notes):
-  scale_notes = []
-  start_index = scale.index(start)
+def get_interval_start(first_note, interval_offset):
+  start_index = scale.index(first_note)
   curr_index = start_index
+  for i in range(interval_offset):
+
+    if curr_index + 1 >= len(scale):
+      curr_index = 0
+    else:
+      curr_index += 1
+  return curr_index
+
+def get_scale_notes(first_note, interval_start_index, total_notes):
+  scale_notes = []
+
+  curr_index = interval_start_index
   for i in range(total_notes):
     scale_notes.append(scale[curr_index])
     if curr_index + 1 >= len(scale):
@@ -16,18 +26,12 @@ scale = ["A", "B", "C#", "D", "E", "F#", "G#"]
 notes = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
 
 intervals = {
-  'Minor Second': 1,
-  'Major Second': 2,
-  'Minor Third': 3,
-  'Major Third': 4, 
-  'Perfect Fourth': 5,
-  'Tri-Tone': 6,
-  'Perfect Fifth': 7,
-  'Minor Sixth': 8,
-  'Major Sixth': 9,
-  'Minor Seventh': 10,
-  'Major Seventh': 11,
-  'Perfect Octave': 12
+  'Major Second': 1,
+  'Major Third': 2, 
+  'Perfect Fourth': 3,
+  'Perfect Fifth': 4,
+  'Major Sixth': 5,
+  'Major Seventh': 6
 }
 
 with open("input.json", "r") as input:
@@ -38,10 +42,11 @@ for phrase in phrases:
   try:
     interval_offset = intervals[phrase['interval']]
     input_notes = phrase['notes']
+    scale_type = phrase['scale']
     total_notes = len(input_notes)
     first_note = input_notes[0]
-    scale_note = notes[notes.index(first_note) + interval_offset]
-    harmony_notes = get_scale_notes(scale_note, total_notes)
+    interval_start_index = get_interval_start(first_note, interval_offset)
+    harmony_notes = get_scale_notes(first_note, interval_start_index, total_notes)
   except Exception:
     print(f"ERROR!! with input -> {input_notes}")
 
